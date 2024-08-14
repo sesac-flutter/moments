8cefde9eb987469f455a2a76df149b7dc59b9d98
S-YUN <candy9won@naver.com>
2024-08-14 18:12:50 +0900

    ui: splash screen 구현. ui 에서 쓰이는 사이즈, 위치의 매직넘버를 어떻게 처리할지 고민 필요

---
+++ lib/presentation/splash/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:moments/ui/common_components/default_btn.dart';
+import 'package:moments/ui/styles/color_styles.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Image.asset(
+            'assets/images/splash_background.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const Positioned(
+            top: 300,
+            child: Text(
+              '가족의 순간이 추억으로 변하는 곳',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: ColorStyles.white),
+            ),
+          ),
+          const Positioned(
+            top: 310,
+            child: Text(
+              'MOMENTS',
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w600,
+                  color: ColorStyles.white),
+            ),
+          ),
+          Positioned(
+              top: 780,
+              child: DefaultBtn(
+                text: '시작하기',
+                onPressed: () {
+                  context.go('/');
+                },
+              )),
+        ],
+      ),
+    );
+  }
+}
